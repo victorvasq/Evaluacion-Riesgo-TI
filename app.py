8321a51dcf81396a5516b43345c3e5a7ed8657e1
victorvasq <victorvasquezrivas@gmail.com>
2024-03-06 17:27:09 -0300

    Update app.py

--- app.py
+++ app.py
@@ -189,7 +189,13 @@ else:
 openai_api_key = st.secrets["api_key"]
 
 
-
+st.markdown("""
+    <style>
+        [data-testid="stHeader"] {
+            display: none !important;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
 
 #######################
